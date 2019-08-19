8c73f19def11cf1f0aa41e5ca95caccb2b0d8ce6
Adam Noyce <45075457+adampnoyce@users.noreply.github.com>
2019-08-19 11:32:05 +0100

    Create send_mail.php

---
+++ samples/send_mail.php
@@ -0,0 +1,42 @@
+<?php
+
+require '../src/SecurePHPMailer.class.php';
+
+$mail = new SecurePHPMailer('user.crt', 'user.key');
+// Send Email
+
+try {
+		
+    //Server settings
+    $mail->SMTPDebug = 1;       // Enable verbose debug output
+    $mail->isSMTP();            // Set mailer to use SMTP
+    $mail->Host       = '';     // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;   // Enable SMTP authentication
+    $mail->Username   = '';     // SMTP username
+    $mail->Password   = '';     // SMTP password
+    $mail->SMTPSecure = 'ssl';  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 465;    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('', 'Mailer');
+    $mail->addAddress('');     
+    $mail->addAddress('');   	// Name is optional
+    $mail->addReplyTo('', 'Information');
+
+    // Format
+    $mail->isHTML(true);
+
+    // Content
+    $mail->Subject = "PHPSecureServer";
+	$mail->Body = '<h3>this is really secret uh</h3>';
+
+	//$mail->sendSignedEmail();
+	//$mail->sendEncryptedEmail('YourCertificate.crt');
+
+
+}  catch (Exception $e) {
+    //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
+?>
